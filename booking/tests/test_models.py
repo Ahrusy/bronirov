48ba9aa62a019115d40f387b453d4bf15eb3e00b
RoKols2017 <RoKols2017@gmail.com>
2025-05-18 21:25:44 +0300

    added file requiremets, the events data model has been added, tests in evets and booking were corrected after model addition

--- booking/tests/test_models.py
+++ booking/tests/test_models.py
@@ -1,19 +1,21 @@
 import pytest
 from django.contrib.auth.models import User
-from events.models import Event, Genre, City
+from events.models import Event, Genre, City, Location  # добавил Location
 from booking.models import Booking, BookingStatus
 from datetime import date
-
 
 @pytest.mark.django_db
 def test_booking_unique_constraint():
     user = User.objects.create_user(username='hacker', password='secret')
     genre = Genre.objects.create(name='Выставка')
     city = City.objects.create(name='Питер')
+    # создаём объект Location
+    location = Location.objects.create(name='Expo', city=city)
+
     event = Event.objects.create(
         title='HackConf',
         event_date=date(2025, 7, 1),
-        location='Expo',
+        location=location,  # передаём объект
         city=city,
         genre=genre,
         ticket_price=1000,
